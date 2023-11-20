8dbd37f378615ce696a55870b7f1da9983b92fed
Abdulkadir Aydemir <111640113+AbdulkadirAydemir@users.noreply.github.com>
2023-11-20 18:33:58 +0300

    Update CustomersController.java

--- Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/CustomersController.java
+++ Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/CustomersController.java
@@ -29,10 +29,16 @@ public class CustomersController {
         customerRepository.save(customer);
     }
 
-    @PutMapping
-    public void update(@RequestBody Customer customer) {
-        Customer customerUpdate = customerRepository.findById(customer.getId()).orElseThrow();
-        customerRepository.save(customerUpdate);
+    @PutMapping("{id}")
+    public void update(@PathVariable int id, @RequestBody Customer updateCustomer) {
+        Customer customerToUpdate = customerRepository.findById(id).orElseThrow();
+        customerToUpdate.setName(updateCustomer.getName());
+        customerToUpdate.setSurname(updateCustomer.getSurname());
+        customerToUpdate.setAddress(updateCustomer.getAddress());
+        customerToUpdate.setPhone(updateCustomer.getPhone());
+        customerToUpdate.setEmail(updateCustomer.getEmail());
+        customerToUpdate.setCarLicense(updateCustomer.getCarLicense());
+        customerRepository.save(customerToUpdate);
     }
 
     @DeleteMapping("{id}")
