2c561e38dd3a858d3d0fa5e29a385103b0d6e9d0
Abdulkadir Aydemir <111640113+AbdulkadirAydemir@users.noreply.github.com>
2023-11-23 19:23:34 +0300

    Update CarsController.java

--- Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/CarsController.java
+++ Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/CarsController.java
@@ -1,9 +1,15 @@
 package com.tobeto.spring_1b.controllers;
 
+import com.tobeto.spring_1b.dtos.requests.car.AddCarRequest;
+import com.tobeto.spring_1b.dtos.requests.car.UpdateCarRequest;
+import com.tobeto.spring_1b.dtos.responses.car.GetCarListResponse;
+import com.tobeto.spring_1b.dtos.responses.car.GetCarResponse;
+import com.tobeto.spring_1b.entities.Brand;
 import com.tobeto.spring_1b.entities.Car;
 import com.tobeto.spring_1b.repositories.CarRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,27 +23,58 @@ public class CarsController {
     }
 
     @GetMapping
-    public List<Car> getAll() {return carRepository.findAll();}
+    public List<GetCarListResponse> getAll() {
+      List<Car> cars = carRepository.findAll();
+      List<GetCarListResponse> responseList = new ArrayList<>();
+
+      for (Car car : cars){
+          GetCarListResponse response = new GetCarListResponse();
+          response.setId(car.getId());
+          response.setYear(car.getYear());
+          response.setName(car.getName());
+          response.setModel(car.getModel());
+          response.setReadyToUse(car.isReadyToUse());
+          response.setRentalPrice(car.getRentalPrice());
+      }
+      return responseList;
+    }
 
     @GetMapping("{id}")
-    public Car getById(@PathVariable int id) {
-        return carRepository.findById(id).orElseThrow();
+    public GetCarResponse getById(@PathVariable int id) {
+        Car car = carRepository.findById(id).orElseThrow();
+
+        GetCarResponse response = new GetCarResponse();
+        response.setYear(car.getYear());
+        response.setName(car.getName());
+        response.setModel(car.getModel());
+        response.setReadyToUse(car.isReadyToUse());
+        response.setRentalPrice(car.getRentalPrice());
+
+        return response;
     }
 
     @PostMapping
-    public void add(@RequestBody Car car){
+    public void add(@RequestBody AddCarRequest request){
+        Car car = new Car();
+        car.setYear(request.getYear());
+        car.setName(request.getName());
+        car.setModel(request.getModel());
+        car.setReadyToUse(request.isReadyToUse());
+        car.setRentalPrice(request.getRentalPrice());
         carRepository.save(car);
     }
 
     @PutMapping("{id}")
-    public void update(@PathVariable int id, @RequestBody Car updateCar) {
-        Car carToUpdate = carRepository.findById(id).orElseThrow();
-        carToUpdate.setName(updateCar.getName());
-        carToUpdate.setModel(updateCar.getModel());
-        carToUpdate.setYear(updateCar.getYear());
-        carToUpdate.setReadyToUse(updateCar.isReadyToUse());
-        carToUpdate.setRentalPrice(updateCar.getRentalPrice());
-        carRepository.save(carToUpdate);
+    public void update(@PathVariable int id, @RequestBody UpdateCarRequest updateCarRequest) {
+        Car car = new Car();
+        carRepository.findById(id).orElseThrow();
+        car.setId(updateCarRequest.getId());
+        car.setName(updateCarRequest.getName());
+        car.setModel(updateCarRequest.getModel());
+        car.setYear(updateCarRequest.getYear());
+        car.setReadyToUse(updateCarRequest.isReadyToUse());
+        car.setRentalPrice(updateCarRequest.getRentalPrice());
+        carRepository.save(car);
     }
 
     @DeleteMapping("{id}")
