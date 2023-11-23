95295362b227c4213cb76465ff55c7c1537464ea
Abdulkadir Aydemir <111640113+AbdulkadirAydemir@users.noreply.github.com>
2023-11-23 19:22:44 +0300

    Update BrandsController.java

--- Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/BrandsController.java
+++ Hibernate-ORM/src/main/java/com/tobeto/spring_1b/controllers/BrandsController.java
@@ -1,9 +1,14 @@
 package com.tobeto.spring_1b.controllers;
 
+import com.tobeto.spring_1b.dtos.requests.brand.AddBrandRequest;
+import com.tobeto.spring_1b.dtos.requests.brand.UpdateBrandRequest;
+import com.tobeto.spring_1b.dtos.responses.brand.GetBrandListResponse;
+import com.tobeto.spring_1b.dtos.responses.brand.GetBrandResponse;
 import com.tobeto.spring_1b.entities.Brand;
 import com.tobeto.spring_1b.repositories.BrandRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,24 +22,47 @@ public class BrandsController {
     }
 
     @GetMapping
-    public List<Brand> getAll() {return brandRepository.findAll();}
+    public List<GetBrandListResponse> getAll() {
+        List<Brand> brands = brandRepository.findAll();
+        List<GetBrandListResponse> responseList = new ArrayList<>();
+
+        for (Brand brand : brands) {
+            GetBrandListResponse response = new GetBrandListResponse();
+            response.setId(brand.getId());
+            response.setName(brand.getName());
+            response.setCountry(brand.getCountry());
+            responseList.add(response);
+        }
+        return responseList;
+    }
+
 
     @GetMapping("{id}")
-    public Brand getById(@PathVariable int id) {
-        return brandRepository.findById(id).orElseThrow();
+    public GetBrandResponse getById(@PathVariable int id) {
+        Brand brand = brandRepository.findById(id).orElseThrow();
+
+        GetBrandResponse response = new GetBrandResponse();
+        response.setName(brand.getName());
+        response.setCountry(brand.getCountry());
+        return response;
     }
 
     @PostMapping
-    public void add(@RequestBody  Brand brand){
+    public void add(@RequestBody AddBrandRequest request){
+        Brand brand = new Brand();
+        brand.setName(request.getName());
+        brand.setCountry(request.getCountry());
         brandRepository.save(brand);
     }
 
     @PutMapping("{id}")
-    public void update(@PathVariable int id, @RequestBody Brand updateBrand) {
-        Brand brandToUpdate = brandRepository.findById(id).orElseThrow();
-        brandToUpdate.setName(updateBrand.getName());
-        brandToUpdate.setCountry(updateBrand.getCountry());
-        brandRepository.save(brandToUpdate);
+    public void update(@PathVariable int id, @RequestBody UpdateBrandRequest updateBrandRequest) {
+        Brand brand = new Brand();
+        brandRepository.findById(brand.getId()).orElseThrow();
+        brand.setId(updateBrandRequest.getId());
+        brand.setName(updateBrandRequest.getName());
+        brand.setCountry(updateBrandRequest.getCountry());
+        brandRepository.save(brand);
     }
 
     @DeleteMapping("{id}")
